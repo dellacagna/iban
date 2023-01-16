7525f8a6aa2badbda2a1b266ddfecbb97e4b9f13
Giovanni Della Cagna <gdellacagna@mila.com>
2023-01-16 17:15:38 +0100

    Package Scripts

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # IBAN Library
+A simple library for validating, formatting and generating fake IBAN numbers.
+
+This library follows the [ISO 13616 IBAN Registry technical specification](https://www.swift.com/standards/data-standards/iban-international-bank-account-number) and supports 82 countries.
+
+## Usage
+### NodeJS
+```
+import { IBAN } from '../index';  
+let iban = new IBAN('GB82 WEST 1234 5698 7654 32');
+if (iban.isValid()) {
+    console.log('The IBAN is valid)
+}
+```
--- src/__tests__/IBAN.test.ts
+++ src/__tests__/IBAN.test.ts
@@ -1,11 +1,11 @@
 import { IBAN } from '../index';
 
 test('isValid IBAN', () => {
-    let ibanObj = new IBAN('GB82 WEST 1234 5698 7654 32');
-    expect(ibanObj.isValid()).toBe(true);
+    let iban = new IBAN('GB82 WEST 1234 5698 7654 32');
+    expect(iban.isValid()).toBe(true);
 });
 
 test('isInvalid IBAN', () => {
-    let ibanObj = new IBAN('GB82 WEST 1234 5698 7654 ');
-    expect(ibanObj.isValid()).toBe(false);
+    let iban = new IBAN('GB82 WEST 1234 5698 7654 ');
+    expect(iban.isValid()).toBe(false);
 });
